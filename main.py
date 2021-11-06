286614d6eb84ad07d613b23abda1224d6a64c425
Nixon Hanna <nixonhanna95@gmail.com>
2021-11-05 20:43:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#Factoring
+#Given a graph find the equation
+#Quadratic formula
+#Line of best fit
+#Systems of Equations
+#Solving single variable equations
+#Inequalities
+#Completing the Squre
+#Sequences
+#Exponetials
+import tkinter as tk
