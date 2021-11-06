605b971f97acf449dcd78e6821205def408551a0
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2021-11-06 13:49:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+#Factoring
 #Given a graph find the equation
+#Quadratic formula
 #Line of best fit
+#Systems of Equations
+#Solving single variable equations
+#Inequalities
+#Completing the Squre
 #Sequences
 #Exponetials
+import tkinter as tk 
 from tkinter import *
 import matplotlib.pyplot as plt
 from tkinter import ttk
@@ -38,7 +45,25 @@ def seq():
     print("hi")
 
 def about():
-    print("hi")
+    sectionTitleText = Text(window, height = 15, width = 61, background='#dadde3', borderwidth = 0, font = ("Helvetica", 15))
+    sectionTitleText.place(x=240, y=25)
+    sectionTitleText.delete(1.0, "end")
+    sectionTitleText.insert(1.0, "About AlgeBRO:"+
+                                "\n " +
+                                "\nOur programs goals are to tutor students in the subject of Algebra. We offer a variety" + 
+                                "\nof questions surrounding key ideas like solving equations, graphs, and multiple" + 
+                                "\nfunctions and allow students to get tips if they got an answer wrong. We also have" + 
+                                "\nour program translated into multiple languages so students from different countries" +  
+                                "\ncan also get help in the universal language of math!" +
+                                "\n " +
+                                "\nHow To Use AlgeBRO:" +
+                                "\n " +
+                                "\n1. Select the skill you want to practice on the side bar menu" +
+                                "\n2. Attempt to solve the problems given." +
+                                "\n3. If you are unable to solve it or just need a nudge in the right direction, just click the hint button" +
+                                "\n4. Enjoy learning and improving up on your math skills!")
+
+#Our programs goals are to tutor students in the subject of Algebra. We offer a variety of questions surrounding key ideas like solving equations, graphs, and multiple functions. We offer multiple questions in each category and allow students to get tipes if they got an answer wrong. We also have our program translated into multiple languages so students from different countries can also get help in the universal language of math!
 
 def english():
     print("hi")
@@ -58,7 +83,7 @@ def arabic():
 def russian():
     print("hi")
 
-  
+
 
 
 window = Tk()
@@ -123,12 +148,6 @@ russianBtn.place(x = 745, y = 475)
 
 separator = ttk.Separator(window, orient='vertical')
 separator.place(relx=0.29, rely=0, relwidth=0.001, relheight=1)
-
-sectionTitleText = Text(window, height=1, font = ("Helvetica", 20))
-sectionTitleText.place(x=250,y=20)
-
-entry = Entry(window)
-entry.place(x = 550, y = 445)
 
 
 window.mainloop()
